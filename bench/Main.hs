7b5ddd9f2a2a987fb722e3582156a9449a36ebc2
Julian K. Arni <jkarni@gmail.com>
2016-08-25 10:08:56 -0300

    initial benchmark work

---
+++ bench/Main.hs
@@ -0,0 +1,48 @@
+module Main where
+
+import Bookkeeper
+import Criterion.Main
+
+type PersonB = Book '[ "name" :=> String
+    , "age" :=> Int
+    , "age1" :=> Int
+    , "age12" :=> Int
+    , "age15" :=> Int
+    , "age2" :=> Int
+    , "age10" :=> Int
+    , "age4" :=> Int
+    , "age9" :=> Int
+    , "age8" :=> Int
+    , "age3" :=> Int
+    , "age11" :=> Int
+    ]
+data PersonR = PersonR { name ::  String, age :: {-# NOUNPACK #-} Int } deriving (Eq, Show)
+
+pb :: PersonB
+pb = emptyBook
+   & #name =: ""
+   & #age =: 0
+
+pr :: PersonR
+pr = PersonR
+   { name = ""
+   , age = 0
+   }
+
+modB :: Int -> PersonB
+modB 0 = pb
+modB n = modB (n - 1) & #age %: (+ n)
+
+modR :: Int -> PersonR
+modR 0 = pr
+modR n = let p = modR (n - 1) in p { age = age p + n }
+
+main :: IO ()
+main = defaultMain
+  [ bgroup "modB" [ bench "10" $ nf (\x -> get #age $ modB x) 10
+                  , bench "100" $ nf (\x -> get #age $ modB x) 100
+                  ]
+  , bgroup "modR" [ bench "10" $ nf (\x -> age $ modR x) 10
+                  , bench "100" $ nf (\x -> age $ modR x) 100
+                  ]
+  ]
